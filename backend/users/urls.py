a95b994560e7b3079b771868ccb089b972f566e1
Max Bazarov <bazaroffm@gmail.com>
2023-02-24 12:24:37 +0300

    fix djoser urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from .api.views import UserViewSet
-
-router = DefaultRouter()
-router.register('users', UserViewSet, basename='users')
+from django.urls import include, path
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
