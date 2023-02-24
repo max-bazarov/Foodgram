a197e04a53976441f4acca971e22c61bb8152dc1
Max Bazarov <bazaroffm@gmail.com>
2023-02-24 12:23:18 +0300

    add username validation

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -1,9 +1,43 @@
-from rest_framework.serializers import ModelSerializer
+from djoser.serializers import UserCreateSerializer, UserSerializer
+from rest_framework import serializers
 
-from users.models import User
+from core.validators import validate_username
+from users.models import Follow, User
 
 
-class UserSerializer(ModelSerializer):
+class CustomUserSerializer(UserSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'last_name')
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+        )
+
+    def get_is_subscribed(self, obj):
+        if Follow.objects.filter(
+            follower=obj, followed=self.context['request'].user
+        ).exists():
+            return True
+        return False
+
+    def validate_username(self, username):
+        return validate_username(username)
+
+
+class CustomUserCreateSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'id',
+            'password',
+        )
