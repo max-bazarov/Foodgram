c475abac12623159e29631d77e37e5940846dc88
Max Bazarov <bazaroffm@gmail.com>
2023-02-24 12:22:39 +0300

    isort

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,6 +1,7 @@
-from .api.views import IngredientViewSet, RecipeViewSet, TagViewSet
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from .api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipes')
