33222b1f590bb952ce64c6460113331681a3a79e
Max Bazarov <bazaroffm@gmail.com>
2023-02-24 12:24:03 +0300

    add username validation

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,77 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+from core.validators import validate_username
 
 
-class User(AbstractUser):
-    pass
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(('Superuser must have is_staff=True.'))
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(('email address'), unique=True)
+    username = models.CharField(
+        ('username'),
+        max_length=255,
+        unique=True,
+        validators=[validate_username]
+    )
+    first_name = models.CharField(('first name'), max_length=30,)
+    last_name = models.CharField(('last name'), max_length=30,)
+
+    is_active = models.BooleanField(('active'), default=True)
+    is_staff = models.BooleanField(('staff status'), default=False)
+    date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
+
+    objects = UserManager()
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = ('user')
+        verbose_name_plural = ('users')
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followers',
+    )
+    followed = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followed'
+    )
