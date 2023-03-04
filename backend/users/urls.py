e62a36c5125d1ac49931c4bfd1803c64b4f32e29
Max Bazarov <bazaroffm@gmail.com>
2023-03-04 23:01:13 +0300

    add user viewset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from users.api.views import UserViewSet
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
