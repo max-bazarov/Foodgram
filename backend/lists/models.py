7e3387d537240da8a40874546baaec73562bb459
Max Bazarov <bazaroffm@gmail.com>
2023-03-04 22:57:43 +0300

    add favorites and cart

--- backend/lists/models.py
+++ backend/lists/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from recipes.models import Recipe
+
+User = get_user_model()
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='favorites'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+    )
+
+    class Meta:
+        ordering = ['-id']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='favorite user recipe constraint',
+            )
+        ]
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='cart',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='cart',
+    )
+
+    class Meta:
+        ordering = ['-id']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'], name='cart user recipe constraint'
+            )
+        ]
