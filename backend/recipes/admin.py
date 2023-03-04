6ac9437268a1e05744e9c012c04289da2bbc88cc
Max Bazarov <bazaroffm@gmail.com>
2023-03-04 22:58:36 +0300

    add all models to admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -5,8 +5,25 @@ from .models import Ingredient, Recipe, Tag
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'text', 'image', 'cooking_time')
+    list_display = (
+        'name',
+        'author',
+        'count_favorites'
+    )
+    list_filter = ('author', 'name', 'tags')
     empty_value_display = '-пусто-'
+
+    fields = (
+        'name',
+        'cooking_time',
+        'author',
+        'tags',
+        'text',
+        'image',
+    )
+
+    def count_favorites(self, obj):
+        return obj.favorites.count()
 
 
 @admin.register(Tag)
@@ -18,4 +35,6 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+    search_fields = ('name',)
     empty_value_display = '-пусто-'
