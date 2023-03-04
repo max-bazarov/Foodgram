8bab045b096a8ee55bdc844914eb495d1d323d69
Max Bazarov <bazaroffm@gmail.com>
2023-03-04 23:00:16 +0300

    add user model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('email', 'username')
