2aa04f492d387abc2b2d47d607d495a36f2eccc1
Max Bazarov <bazaroffm@gmail.com>
2023-03-04 22:59:46 +0300

    add recipe model ;

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,32 +1,16 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-
-class Recipe(models.Model):
-    author = models.ForeignKey(
-        'users.User',
-        on_delete=models.CASCADE,
-        related_name='recipes'
-    )
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    image = models.ImageField(upload_to='images/')
-    ingredients = models.ManyToManyField(
-        'Ingredient',
-        related_name='recipes',
-        through='RecipeIngredient'
-    )
-    tag = models.ManyToManyField(
-        'Tag',
-        related_name='recipes',
-        through='RecipeTag'
-    )
-    cooking_time = models.IntegerField()
+from core.validators import validate_hex_color
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    color = models.CharField(max_length=200)
+    color = models.CharField(max_length=200, validators=[validate_hex_color])
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -34,26 +18,34 @@ class Ingredient(models.Model):
     measurement_unit = models.CharField(max_length=200)
 
 
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='recipes'
+    )
+    name = models.CharField(
+        max_length=200,
+    )
+    text = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    ingredients = models.ManyToManyField(
+        Ingredient, through='RecipeIngredient', related_name='recipes'
+    )
+    tags = models.ManyToManyField(Tag, related_name='recipes')
+    cooking_time = models.IntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        ordering = ['-id']
+
+
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
-        Recipe,
-        on_delete=models.CASCADE,
+        Recipe, on_delete=models.CASCADE, related_name='ingredient'
     )
     ingredient = models.ForeignKey(
-        Ingredient,
-        on_delete=models.CASCADE,
+        Ingredient, on_delete=models.CASCADE, related_name='recipe'
     )
-    amout = models.IntegerField()
-
-
-class RecipeTag(models.Model):
-    recipe = models.ForeignKey(
-        Recipe,
-        on_delete=models.CASCADE,
-        related_name='tags'
-    )
-    tag = models.ForeignKey(
-        Tag,
-        on_delete=models.CASCADE,
-        related_name='recipe_tags'
+    amount = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0), MaxValueValidator(10000)],
     )
